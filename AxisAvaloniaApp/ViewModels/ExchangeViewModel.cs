8c5356700ad3a5b00da59be5642ce02b8d6ec6cd
Viktor Kassov <kassov@mail.ua>
2022-06-06 13:41:24 +0300

    Toggle Slider

--- AxisAvaloniaApp/ViewModels/ExchangeViewModel.cs
+++ AxisAvaloniaApp/ViewModels/ExchangeViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using AxisAvaloniaApp.UserControls.Models;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +12,67 @@ namespace AxisAvaloniaApp.ViewModels
 {
     public class ExchangeViewModel : ViewModelBase
     {
+        private bool isImportDirection;
+        private ObservableCollection<ComboBoxItemModel> importFromItems;
+        private ComboBoxItemModel selectedImportFromItem;
+        private ObservableCollection<ComboBoxItemModel> exportToItems;
+        private ComboBoxItemModel selectedExportToItem;
+        private bool isSaleOperation;
+        public bool IsImportDirection { get => isImportDirection; set => this.RaiseAndSetIfChanged(ref isImportDirection, value); }  
+        public ObservableCollection<ComboBoxItemModel> ImportFromItems
+        {
+            get => importFromItems;
+            set => this.RaiseAndSetIfChanged(ref importFromItems, value);
+        }
+        public ComboBoxItemModel SelectedImportFromItem { get => selectedImportFromItem; set => this.RaiseAndSetIfChanged(ref selectedImportFromItem, value); }
+        public ObservableCollection<ComboBoxItemModel> ExportToItems
+        {
+            get => exportToItems;
+            set => this.RaiseAndSetIfChanged(ref exportToItems, value);
+        }
+        public ComboBoxItemModel SelectedExportToItem { get => selectedExportToItem; set => this.RaiseAndSetIfChanged(ref selectedExportToItem, value); }
+        public bool IsSaleOperation { get => isSaleOperation; set => this.RaiseAndSetIfChanged(ref isSaleOperation, value); }
+        public ReactiveCommand<Unit, Unit> ChangeDirectionCommand { get; }
+
+        public ExchangeViewModel()
+        {
+            ChangeDirectionCommand = ReactiveCommand.Create(ChangeDirection);
+            ImportFromItems = GetImportFromItemsCollection();
+            ExportToItems = GetExportToItemsCollection();
+        }
+
+        private ObservableCollection<ComboBoxItemModel> GetImportFromItemsCollection()
+        {
+            ObservableCollection<ComboBoxItemModel> result = new ObservableCollection<ComboBoxItemModel>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                ComboBoxItemModel item = new ComboBoxItemModel();
+                item.Key = "item" + i;
+                item.Value = item.Key;
+                result.Add(item);
+            }
+            
+            return result;
+        }
+        private ObservableCollection<ComboBoxItemModel> GetExportToItemsCollection()
+        {
+            ObservableCollection<ComboBoxItemModel> result = new ObservableCollection<ComboBoxItemModel>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                ComboBoxItemModel item = new ComboBoxItemModel();
+                item.Key = "item" + i;
+                item.Value = item.Key;
+                result.Add(item);
+            }
+
+            return result;
+        }
+
+        private void ChangeDirection()
+        {
+            IsImportDirection = !IsImportDirection;
+        }
     }
 }
