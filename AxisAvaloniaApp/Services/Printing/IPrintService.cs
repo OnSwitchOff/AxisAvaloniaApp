6fa01666e2de37e90f320316710c362ac40508d0
Viktor Kassov <kassov@mail.ua>
2022-06-15 13:39:04 +0300

    PrintImageList

--- AxisAvaloniaApp/Services/Printing/IPrintService.cs
+++ AxisAvaloniaApp/Services/Printing/IPrintService.cs
@@ -3,6 +3,7 @@ using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace AxisAvaloniaApp.Services.Printing
     {
         List<string> GetPrinters();
         bool SendByteArrayToPrinter(string printerName, byte[] bytes);
+        bool PrintImageList(string szPrinterName, List<Image> images);
     }
 
 }
--- AxisAvaloniaApp/Services/Printing/WindowsPrintService.cs
+++ AxisAvaloniaApp/Services/Printing/WindowsPrintService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,6 +57,47 @@ namespace AxisAvaloniaApp.Services.Printing
             }
             return true;
            
+        }
+
+        List<Image> Images = new List<Image>();
+        // The Click event is raised when the user clicks the Print button.
+        public bool PrintImageList(string szPrinterName, List<Image> images)
+        {
+            try
+            {
+                Images = images;
+                PrintDocument pd = new PrintDocument();
+                pd.PrinterSettings.PrinterName = szPrinterName;
+                //pd.PrinterSettings.PrintFileName
+                pd.PrintPage += Pd_PrintPage;
+                pd.Print();
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+        // The PrintPage event is raised for each page to be printed.
+        private void Pd_PrintPage(object sender, PrintPageEventArgs ev)
+        {
+            // Create image.
+            //Image newImage = Image.FromFile("SampImag.jpg");
+            Image newImage = Images[0];
+
+            // Create coordinates for upper-left corner of image.
+            float x = 0.0F;
+            float y = 0.0F;
+
+            // Create rectangle for source image.
+            RectangleF srcRect = new RectangleF(0.0F, 0.0F, newImage.Width, newImage.Height);
+            GraphicsUnit units = GraphicsUnit.Pixel;
+
+            // Draw image to screen.
+            ev.Graphics.DrawImage(newImage, x, y, srcRect, units);
+            Images.Remove(Images[0]);
+            ev.HasMorePages = Images.Count > 0;
         }
     }
 }
