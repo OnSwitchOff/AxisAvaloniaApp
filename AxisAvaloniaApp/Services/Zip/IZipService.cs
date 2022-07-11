39dcdb2f70234ca45a910dfd3e3e76f1c34e6257
Viktor <kassov@ukr.net>
2022-07-11 11:56:34 +0300

    ZipService(ForBackUp)

---
+++ AxisAvaloniaApp/Services/Zip/IZipService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxisAvaloniaApp.Services.Zip
+{
+    public interface IZipService
+    {
+        bool CompressFileToZip(string fileName, string destination, string entryName);
+        bool ExtractAllFromZip(string zipName, string destination);
+        Task<bool> CompressFileToZipAsync(string fileName, string destination, string entryName);
+        Task<bool> ExtractAllFromZipAsync(string zipName, string destination);
+    }
+}
